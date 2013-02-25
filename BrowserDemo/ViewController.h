85da25923fc9fc540892e8726518144a01156fcd
Anil Motwani <anilm@Anils-Mac-mini.local>
2013-02-25 11:29:25 +0530

    Initial Commit

---
+++ BrowserDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BrowserDemo
+//
+//  Created by Anil Motwani on 2/25/13.
+//  Copyright (c) 2013 indusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BrowserDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BrowserDemo
+//
+//  Created by Anil Motwani on 2/25/13.
+//  Copyright (c) 2013 indusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
